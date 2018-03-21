1ed0004a2c53d7f93a0bcf31c56ce148a38d0984
PadMan <Paderman@web.de>
2018-03-21 23:46:27 +0100

    Visual Studio 15

--- MarqueeingTextBlock/MainPage.xaml.cs
+++ MarqueeingTextBlock/MainPage.xaml.cs
@@ -1,20 +1,16 @@
-﻿using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
 
 namespace MarqueeingTextBlock
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage
     {
         public MainPage()
         {
-            this.InitializeComponent();
-
-            this.NavigationCacheMode = NavigationCacheMode.Required;
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Required;
         }
 
         /// <summary>
