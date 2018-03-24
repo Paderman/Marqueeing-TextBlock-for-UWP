6e3b5424c2784c7bc7a05e23d4ffc880475e4a9f
PadMan <Paderman@web.de>
2018-03-24 03:23:20 +0100

    Add files via upload

---
+++ Visual_C++/MarqueeingTextBlock/App.xaml.cpp
@@ -0,0 +1,78 @@
+﻿//
+// App.xaml.cpp
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace MarqueeingTextBlock;
+
+using namespace Platform;
+using namespace Windows::ApplicationModel;
+using namespace Windows::ApplicationModel::Activation;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Interop;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+App::App()
+{
+    InitializeComponent();
+    Suspending += ref new SuspendingEventHandler(this, &App::OnSuspending);
+}
+
+void App::OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEventArgs^ e)
+{
+    auto rootFrame = dynamic_cast<Frame^>(Window::Current->Content);
+
+    if (rootFrame == nullptr)
+    {
+
+        rootFrame = ref new Frame();
+
+        rootFrame->NavigationFailed += ref new Windows::UI::Xaml::Navigation::NavigationFailedEventHandler(this, &App::OnNavigationFailed);
+
+        if (e->PreviousExecutionState == ApplicationExecutionState::Terminated)
+        {
+
+        }
+
+        if (e->PrelaunchActivated == false)
+        {
+            if (rootFrame->Content == nullptr)
+            {
+                rootFrame->Navigate(TypeName(MainPage::typeid), e->Arguments);
+            }
+            Window::Current->Content = rootFrame;
+            Window::Current->Activate();
+        }
+    }
+    else
+    {
+        if (e->PrelaunchActivated == false)
+        {
+            if (rootFrame->Content == nullptr)
+            {
+                rootFrame->Navigate(TypeName(MainPage::typeid), e->Arguments);
+            }
+            Window::Current->Activate();
+        }
+    }
+}
+
+void App::OnSuspending(Object^ sender, SuspendingEventArgs^ e)
+{
+    (void) sender;  // Nicht verwendeter Parameter
+    (void) e;   // Nicht verwendeter Parameter
+}
+
+void App::OnNavigationFailed(Platform::Object ^sender, Windows::UI::Xaml::Navigation::NavigationFailedEventArgs ^e)
+{
+    throw ref new FailureException("Failed to load Page " + e->SourcePageType.Name);
+}
---
+++ Visual_C++/MarqueeingTextBlock/MainPage.xaml.h
@@ -0,0 +1,19 @@
+﻿//
+// MainPage.xaml.h
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace MarqueeingTextBlock
+{
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+
+	private:
+		void Page_Loaded(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+	};
+}
